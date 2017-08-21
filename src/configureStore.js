36b651cdf679f7966c7578039bbadfa9e0b7c04a
juan ramino <juan.ramino@gmail.com>
2017-08-21 12:06:18 +0200

    Chapter 12 - Wrapping dispatch() to Log Actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,12 +3,35 @@ import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 import { loadState, saveState } from './localStorage';
 
+const addLogingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(
     todoApp,
     persistedState
   );
+
+  if (process.env.NODE_ENV !== 'production') {
+    // sostituisco la funzione dispatch di redux con questa che aggiunge i logs
+    store.dispatch = addLogingToDispatch(store);
+  }
 
   store.subscribe(throttle(() => {
     saveState({
