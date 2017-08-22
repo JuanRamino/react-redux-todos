061d934d1f95c29be834800a50820a57a53333d5
juan ramino <juan.ramino@gmail.com>
2017-08-22 11:42:15 +0200

    Chapter 22 - Dispatching Actions Asynchronously with Thunks

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,12 +18,15 @@ const receiveTodos = (filter, response) => ({
   response,
 });
 
-export const fetchTodos = (filter) =>
-  api.fetchTodos(filter).then(response =>
-    receiveTodos(filter, response)
-  );
-
-export const requestTodos = (filter) => ({
+const requestTodos = (filter) => ({
   type: 'REQUEST_TODOS',
   filter,
 });
+
+export const fetchTodos = (filter) => (dispatch) => {
+  dispatch(requestTodos(filter));
+
+  return api.fetchTodos(filter).then(response => {
+    dispatch(receiveTodos(filter, response));
+  });
+};
